481f8ed17a0777b0f5bb79a81946f7b618a63fe0
aKuch <aKuch@wp.pl>
2022-03-20 23:36:25 +0100

    3 pliki dodane

---
+++ 04-typy-danych.py
@@ -0,0 +1,32 @@
+### str
+# name = "Adam"
+# name2 = 'Adam 2'
+# name3 = """Adam
+# 1
+# 2
+# 3"""
+# print(name3)
+
+###int float
+# a = 10
+# b = 2.5
+# print(a + b)
+#
+# c = 100_000
+# print(c)
+
+### bool
+# is_sky_blue = True  # musi być z dużej
+# is_sun_blue = False
+# # print(is_sky_blue)
+# # print(is_sun_blue)
+#
+# print(type(name))
+# print(type(a))
+# print(type(is_sun_blue))
+
+# przekształcanie
+first = 2
+second = "3"
+print(first + int(second))  # 5
+print(str(first) + second)  # 23
